c08d2e12b0fab8a78496c2fee0e5f5aced2d0496
Roman <Roman.badiere@gmail.com>
2023-12-04 09:25:11 -0700

    deletion of redundant commented out code

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,6 @@ fn main() {
         .run();
 }
 
-// fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-//     commands.spawn(Camera2dBundle::default());
-//     commands.spawn(SpriteBundle {
-//         texture: asset_server.load("icon.png"),
-//         ..Default::default()
-//     });
-// }
-
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut camera = Camera2dBundle::default();
     camera.projection.scaling_mode = ScalingMode::AutoMin {
